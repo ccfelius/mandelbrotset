d18f2fc6e2a1706c24fe05e5f140c437bc4d9741
ccfelius <lottefelius@Hotmail.com>
2020-11-15 14:50:46 +0100

    estimated area right

--- monte.py
+++ monte.py
@@ -15,26 +15,14 @@ xrange = xmax - xmin
 yrange = ymax - ymin
 area = xrange*(yrange*-1j).real
 
-def estimation_area():
-    # we need to create a function that estimates the area of the mandelbrot
-    # iets met pixels in mandelbrot en erbuiten?
-
-    #### STEPS:
-    # Calculate # pixels (all combinations of nx, ny
-    # Calculate # pixels in mandelbrot
-
-    # dan is als het goed is de area van mandelbrot geschat?
-    return 0
-
-
 def calculation(c,  maxiter = 1000, simulation=False, z = 0.0j):
 
     if not simulation:
         for i in range(maxiter):
             z = z * z + c
             if np.sqrt((z.real * z.real) + (z.imag * z.imag)) >= 2:
-                return i, 1
-        return maxiter, 0
+                return i, 0
+        return maxiter, 1
 
     else:
         for i in range(maxiter):
@@ -62,6 +50,13 @@ def mandelbrot(xmax=1.5,xmin=-2.5,ymax=1.5j,ymin=-1.5j, maxiter = 100, step=0.01
             m[iy, ix, 2], binary = calculation(c, maxiter)
             cumsum += binary
 
+    fig = plt.figure()
+    plt.imshow(m[:, :, 2], cmap='hot',
+               extent=[xmin, xmax, (ymin * -1j).real, (ymax * -1j).real])
+    plt.ylabel('Imaginary')
+    plt.xlabel('Real')
+    plt.title('Mandelbrot Plot')
+
     print(f"Total pixels: {pixels}")
     est_area = cumsum/pixels * area
     print(f'Time elapsed for estimating the area with i = {maxiter} is {time.time() - start} seconds.')
@@ -69,29 +64,34 @@ def mandelbrot(xmax=1.5,xmin=-2.5,ymax=1.5j,ymin=-1.5j, maxiter = 100, step=0.01
     return est_area
 
 
-def random_sampling(xmax=1.5,xmin=-2.5,ymax=1.5j,ymin=-1.5j, maxiter=100, samples = 1000):
+def random_sampling(xmax=1.5,xmin=-2.5,ymax=1.5j,ymin=-1.5j, maxiter=100, samples = 1000, step = 0.01):
     start = time.time()
     in_mandelbrot = 0
     count = 0
 
     while count < samples:
-        # create random sample
+        # create random sample in complex plane
         y_sample = random.uniform(ymin, ymax)
-        x_sample = random.uniform(xmin, xmax)
-        sample = x_sample + y_sample
+        nx = int((xmax - xmin) / step)
+        for ix in range(nx):
+            x_val = xmin + ix * step
+            sample = x_val + y_sample
 
-        # check if in mandelbrotset
-        if calculation(sample, maxiter, simulation = True):
-            in_mandelbrot +=1
-        count += 1
+            # check if in mandelbrotset
+            if calculation(sample, maxiter, simulation = True):
+                in_mandelbrot +=1
+            count += 1
 
     sample_area = in_mandelbrot/samples * (xrange*(yrange*-1j).real)
-    # print(in_mandelbrot, samples, xrange*(yrange*-1j).real)
     print(f'Time elapsed for sampling with s = {samples} , i = {maxiter} is {time.time() - start} seconds.')
     print(f"Sample area = {sample_area}")
     return sample_area
 
+
 samples = 10000
+simulations = 10
+p_value = 0.95
+
+estimation = mandelbrot(xmax, xmin, ymax, ymin, maxiter)
 r_sampling = random_sampling(xmax, xmin, ymax, ymin, maxiter, samples)
-m_estimation = mandelbrot(xmax, xmin, ymax, ymin, maxiter)
 
