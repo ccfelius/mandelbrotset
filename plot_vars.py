4d9404bcc2a83f5d77e0c1c14e4f843c2e303ec8
Amber den Hollander <amber.guinevere@gmail.com>
2020-11-17 18:41:50 +0100

    laatste versei

--- plot_vars.py
+++ plot_vars.py
@@ -3,12 +3,6 @@ import scipy.stats as st
 import math
 import numpy as np
 
-# calculate the variance
-def var(array, mean):
-    var1 = 0
-    for i in array:
-        var1 += (i - mean)**2
-    return var1 / len(array)
 
 # calculate confidence interval
 def conf_int(mean, var, n, p=0.95):
@@ -18,7 +12,8 @@ def conf_int(mean, var, n, p=0.95):
     alambda = (zval*sigma)/math.sqrt(n)
     min_lambda = mean - alambda
     plus_lambda = mean + alambda
-    return f"Confidence interval: [{min_lambda} < X < {plus_lambda}] with p = {p}"
+    return min_lambda, plus_lambda
+
 
 # Simulations initial values
 z = 0.0j
@@ -28,50 +23,88 @@ xmin= -2.0
 ymax= 1.1j
 ymin= -1.1j
 
-simulations = 50
-p_value = 0.95
+p_value = 0.95 
 
 # run simulation
-def conduct_experiment(it, samples):
+def conduct_experiment(it, samples, simulations):
     outcomes = []
     for n in range(1, simulations+1):
         r_sampling = random_sampling(xmax, xmin, ymax, ymin, it, samples)
-        # print(f"Simulation {n} gives an area of {r_sampling}")
         outcomes.append(r_sampling)
+    
+    return outcomes
 
-    var1 = var(outcomes, EX)
-    # print(f"Variance: {var1}")
-    # print(conf_int(EX, var1, simulations, p=0.95))
-    return var1
+#################
 
-# check var change for iterations increase
-maxiters = np.array([100, 200, 300, 400, 500])
-maxiter_vars= []
+# check errors change for iterations increase
+simulations = 20
+maxiters =  np.arange(100, 5001, 100).tolist()
+errors = []
+samples = 5000
+max_i = np.mean(conduct_experiment(5000, samples, simulations))
+print(max_i)
+id
 for it in maxiters:
-    EX = mandelbrot(xmax, xmin, ymax, ymin, it) 
-    maxiter_vars.append(conduct_experiment(it, samples=10000))
+    print(it)
+    errors.append((abs(np.mean(conduct_experiment(it, samples, simulations)) - max_i)))
 
-plt.plot(maxiters, maxiter_vars)
-plt.ylabel('Variance')
+
+plt.plot(maxiters, errors)
+plt.ylabel('Error')
 plt.xlabel('Number of iterations')
-# plt.title('Variance over iterations Plot')
 plt.show()
 plt.savefig('iterations.png')
 plt.close()
 
-# check variance change for samples increase
-samples = np.array([100, 1000, 10000, 100000])
-samples_vars= []
+##########################
+
+# check mean change for samples increase
+
+simulations = 20
+samples = np.arange(100, 10001, 100).tolist() 
+samples_mean, ci_lower, ci_higher = [], [], []
 maxiter = 200
+
 for sample in samples:
-    EX = mandelbrot(xmax, xmin, ymax, ymin, maxiter)
-    samples_vars.append(conduct_experiment(maxiter, sample))
+    print(sample)
+    outcomes= (conduct_experiment(maxiter, sample, simulations))
+    mean= np.mean(outcomes)
+    samples_mean.append(mean)
+    lower, higher = conf_int(np.mean(outcomes), np.var(outcomes), len(outcomes), p=0.95)
+    ci_lower.append(lower)
+    ci_higher.append(higher)
 
-plt.xscale('log')
-plt.plot(samples, samples_vars)
-plt.ylabel('Variance')
+plt.plot(samples, samples_mean)
+plt.fill_between(samples, ci_lower, ci_higher, color='b', alpha=.1)
+
+plt.ylabel('Mean')
 plt.xlabel('Number of samples')
-#plt.title('Variance over samples Plot')
-#plt.show()
+plt.show()
 plt.savefig('samples.png')
+plt.close()
 
+# #################
+
+# check mean change for simulations increase
+simulations_mean, ci_lower, ci_higher = [], [], []
+samples = 1000
+maxiter = 200
+simulations = np.arange(100, 3001, 100).tolist() 
+
+for i in simulations:
+    print(i)
+    outcomes= (conduct_experiment(maxiter, samples, i))
+    mean = np.mean(outcomes)
+    simulations_mean.append(mean)
+    lower, higher = conf_int(np.mean(outcomes), np.var(outcomes), len(outcomes), p=0.95)
+    ci_lower.append(lower)
+    ci_higher.append(higher)
+
+plt.plot(simulations, simulations_mean)
+plt.fill_between(simulations, ci_lower, ci_higher, color='b', alpha=.1)
+
+plt.ylabel('Mean')
+plt.xlabel('Number of simulations')
+plt.show()
+plt.savefig('simulations.png')
+plt.close()
